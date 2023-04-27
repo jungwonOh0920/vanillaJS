75f29537e7ed935fa78593fb6bb1c41fca7397ec
Jay Oh <jungwon.oh0920@gmail.com>
2023-04-27 12:38:36 -0700

    first commit

---
+++ todo/index.js
@@ -0,0 +1,31 @@
+const handleClick = () => {
+    const input = document.getElementById('input')
+    const ul = document.getElementById('ul')
+
+    // create list
+    const li = document.createElement('li')
+    const checkbox = document.createElement('input')
+    checkbox.setAttribute('type', 'checkbox')
+
+    // handle checkbox click
+    checkbox.onclick = () => {
+        handleCheckbox(checkbox)
+    }
+
+    // append
+    li.appendChild(document.createTextNode(input.value))
+    li.appendChild(checkbox)
+    ul.appendChild(li)
+
+    // clean input & re-focus
+    input.value = ''
+    input.focus()
+}
+
+const handleCheckbox = (checkbox) => {
+    if (checkbox.checked) {
+        checkbox.parentElement.style.textDecoration = 'line-through'
+    } else {
+        checkbox.parentElement.style.textDecoration = 'none'
+    }
+}
