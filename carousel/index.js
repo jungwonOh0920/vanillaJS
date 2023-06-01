0ba2b2c9a0de727a50a7840a2ee0e44a4d8f4ebe
Jay Oh <jungwon.oh0920@gmail.com>
2023-06-01 10:40:09 -0700

    carousel review

--- carousel/index.js
+++ carousel/index.js
@@ -1,24 +1,31 @@
 let slideIdx = 0
+const slides = document.getElementsByClassName('slide-container')
+const dots = document.getElementsByClassName('dot')
+const SLIDES_NUM = slides.length
 
-const showSlide = () => {
-    const slides = document.getElementsByClassName('slide-container')
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = 'none'
+const showSlide = (idx) => {
+    for (let i = 0; i < SLIDES_NUM; i++) {
+        const slide = slides[i]
+        const dot = dots[i]
+        slide.classList.remove('active')
+        dot.classList.remove('active')
     }
 
-    slides[slideIdx].style.display = 'block'
+    slides[idx].classList.add('active')
+    dots[idx].classList.add('active')
 }
+
 
 const decreaseSlideIdx = () => {
     slideIdx--
-    if (slideIdx < 0) slideIdx = 2
-    showSlide()
+    if (slideIdx < 0) slideIdx = SLIDES_NUM - 1
+    showSlide(slideIdx)
 }
 
 const increaseSlideIdx = () => {
     slideIdx++
-    if (slideIdx > 2) slideIdx = 0
-    showSlide()
+    if (slideIdx >= SLIDES_NUM) slideIdx = 0
+    showSlide(slideIdx)
 }
 
-showSlide()
+showSlide(0)
