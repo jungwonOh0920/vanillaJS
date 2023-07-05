797462e0a4c586ea58ffb0568ea0df25f773313a
Jay Oh <jungwon.oh0920@gmail.com>
2023-07-05 10:24:56 -0700

    handled event bubbling and capturing.

--- other/modal/index.js
+++ other/modal/index.js
@@ -2,3 +2,14 @@ const toggleModal = () => {
     const modalEle = document.getElementsByClassName('modal-container')[0]
     modalEle.classList.toggle('active')
 }
+
+const outer = (e) => {
+    e.stopPropagation()
+    console.log('outer clicked')
+    toggleModal()
+}
+
+const inner = (e) => {
+    e.stopPropagation()
+    console.log('inner clicked');
+}
