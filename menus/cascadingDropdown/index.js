cebe9665810ae4773550d21bc2d59363c3f4ebd7
Jay Oh <jungwon.oh0920@gmail.com>
2023-07-07 15:47:27 -0700

    cascading dropdown

--- menus/cascadingDropdown/index.js
+++ menus/cascadingDropdown/index.js
@@ -3,13 +3,28 @@ const subjectObject = {
     'back-end': ['php', 'sql']
 }
 
+// add options in subject select element
 const init = () => {
     const subjectSelectEle = document.getElementById('subjectSelect')
 
-    for (let x in subjectObject) {
-        subjectSelectEle.options[subjectSelectEle.options.length] = new Option(x, x)
+    // loop thru the subjectObj then get the keys
+    let idx = 1
+    for (let key in subjectObject) {
+        // add an option with text and value
+        subjectSelectEle.options[idx] = new Option(key, key)
+        idx++
     }
 }
 
 init()
 
+const handleChange = (e) => {
+    const topicSelectEle = document.getElementById('topicSelect')
+    topicSelectEle.length = 1
+
+    //loop thru the value for the given key(e.target.value)
+    const key = e.target.value
+    for (let i in subjectObject[key]) {
+        topicSelectEle.options[+i + 1] = new Option(subjectObject[key][i])
+    }
+}
