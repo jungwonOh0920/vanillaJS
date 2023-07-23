e20f82cb5509a6e55c212bfc5fdc8ca6d3917c4c
Jay Oh <jungwon.oh0920@gmail.com>
2023-07-23 11:19:28 -0700

    current num and img alt display accordingly.

--- images/lightbox/index.js
+++ images/lightbox/index.js
@@ -1,3 +1,34 @@
-const toggleModal = () => {
-    document.getElementsByClassName('modal-container')[0].classList.toggle('active')
+const IMG_SOURCES = ['./images/img_lights_wide.jpg', './images/img_mountains.jpg', './images/img_nature.jpg', './images/img_snow.jpg']
+const images = document.getElementsByClassName('img')
+
+let imageIdx = 0
+
+const changeImg = () => {
+    const SlideImg = document.getElementsByClassName('slide-img')[0]
+    SlideImg.src = IMG_SOURCES[imageIdx]
+    SlideImg.alt = images[imageIdx].alt
+
+    document.getElementsByClassName('slider-desc')[0].innerHTML = SlideImg.alt
+}
+
+const openModal = (idx) => {
+    document.getElementsByClassName('modal-container')[0].classList.add('active')
+    imageIdx = idx
+    changeImg()
+}
+
+const closeModal = () => {
+    document.getElementsByClassName('modal-container')[0].classList.remove('active')
+}
+
+const moveLeft = () => {
+    imageIdx -= 1
+    if (imageIdx < 0) imageIdx = IMG_SOURCES.length - 1
+    changeImg()
+}
+
+const moveRight = () => {
+    imageIdx += 1
+    if (imageIdx > IMG_SOURCES.length - 1) imageIdx = 0
+    changeImg()
 }
