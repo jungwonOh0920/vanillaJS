4026db7200069673234037fc672b8ddf84a8690a
Jay Oh <jungwon.oh0920@gmail.com>
2023-07-28 08:58:27 -0700

    refactored

--- images/lightbox/index.js
+++ images/lightbox/index.js
@@ -1,4 +1,4 @@
-const IMG_SOURCES = ['/assets/images/img_lights_wide.jpg', '/assets/images/img_mountains.jpg', '/assets/images/img_nature.jpg', '/assets/images/img_snow.jpg']
+const IMG_SOURCES = ['/assets/images/lights_wide.jpg', '/assets/images/mountains.jpg', '/assets/images/nature.jpg', '/assets/images/snow.jpg']
 const images = document.getElementsByClassName('img')
 
 let imageIdx = 0
