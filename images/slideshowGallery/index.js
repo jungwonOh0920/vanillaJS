7df8bed46e0f956ae230681958e90b61d5c5b3f0
Jay Oh <jungwon.oh0920@gmail.com>
2023-07-28 11:08:22 -0700

    left/right buttons work on slider.

---
+++ images/slideshowGallery/index.js
@@ -0,0 +1,24 @@
+const IMAGES = ['/assets/images/lights_wide.jpg', '/assets/images/mountains.jpg', '/assets/images/nature.jpg', '/assets/images/snow.jpg', '/assets/images/terre_wide.jpeg', '/assets/images/woods_wide.jpeg']
+
+let SLIDE_IMG_IDX = 0
+const slideImg = document.getElementsByClassName('slide-img')[0]
+
+const goLeft = () => {
+    SLIDE_IMG_IDX--
+
+    if (SLIDE_IMG_IDX < 0) {
+        SLIDE_IMG_IDX = IMAGES.length - 1
+    }
+
+    slideImg.src = IMAGES[SLIDE_IMG_IDX]
+}
+
+const goRight = () => {
+    SLIDE_IMG_IDX++
+
+    if (SLIDE_IMG_IDX > IMAGES.length - 1) {
+        SLIDE_IMG_IDX = 0
+    }
+
+    slideImg.src = IMAGES[SLIDE_IMG_IDX]
+}
