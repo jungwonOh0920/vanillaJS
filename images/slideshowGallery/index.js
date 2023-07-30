270bc29b3067e3d8206ee945850f549d07343f29
Jay Oh <jungwon.oh0920@gmail.com>
2023-07-30 16:34:57 -0700

    slides work

--- images/slideshowGallery/index.js
+++ images/slideshowGallery/index.js
@@ -1,27 +1,16 @@
 const IMAGES = ['/assets/images/lights_wide.jpg', '/assets/images/mountains.jpg', '/assets/images/nature.jpg', '/assets/images/snow.jpg', '/assets/images/terre_wide.jpeg', '/assets/images/woods_wide.jpeg']
 
 let SLIDE_IMG_IDX = 0
-const slideImg = document.getElementsByClassName('slide-img')[0]
 
-const goLeft = () => {
-    SLIDE_IMG_IDX--
-
-    if (SLIDE_IMG_IDX < 0) {
-        SLIDE_IMG_IDX = IMAGES.length - 1
-    }
-
-    slideImg.src = IMAGES[SLIDE_IMG_IDX]
-    handelNavNum()
-}
-
-const goRight = () => {
-    SLIDE_IMG_IDX++
-
+const rotateSlide = (num) => {
+    SLIDE_IMG_IDX += num
     if (SLIDE_IMG_IDX > IMAGES.length - 1) {
         SLIDE_IMG_IDX = 0
     }
-
-    slideImg.src = IMAGES[SLIDE_IMG_IDX]
+    if (SLIDE_IMG_IDX < 0) {
+        SLIDE_IMG_IDX = IMAGES.length - 1
+    }
+    activeSlide(SLIDE_IMG_IDX)
     handelNavNum()
 }
 
@@ -29,3 +18,15 @@ const handelNavNum = () => {
     const navNumEl = document.getElementsByClassName('nav-num')[0]
     navNumEl.innerHTML = SLIDE_IMG_IDX + 1 + '/' + IMAGES.length
 }
+
+const activeSlide = (idx) => {
+    const SLIDES = document.getElementsByClassName('slide')
+
+    for (let i = 0; i < SLIDES.length; i++) {
+        SLIDES[i].classList.remove('active')
+    }
+
+    SLIDES[idx].classList.add('active')
+}
+
+activeSlide(0)
