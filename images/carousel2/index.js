e0b60117c4c25595a29644d6ec21aad26468e4d5
Jay Oh <jungwon.oh0920@gmail.com>
2023-08-17 14:31:25 -0700

    carousel 2 done

--- images/carousel2/index.js
+++ images/carousel2/index.js
@@ -1,9 +1,25 @@
-const containerEle = document.getElementsByClassName('container')[0]
-
+const innerContainerEle = document.getElementsByClassName('inner-container')[0]
 const goLeft = () => {
-
+    innerContainerEle.scrollLeft -= 1000;
+    handleArrows()
 }
 
 const goRight = () => {
-    containerEle.scrollLeft += 20;
+    innerContainerEle.scrollLeft += 1000;
+    handleArrows()
+}
+
+const handleArrows = () => {
+    const innerContainerEle = document.getElementsByClassName('inner-container')[0]
+    console.log(innerContainerEle.scrollLeft)
+
+    if (innerContainerEle.scrollLeft >= 1000 && innerContainerEle.scrollLeft < 2000) {
+        document.getElementsByClassName('left-arrow')[0].classList.remove('hidden')
+    } else if (innerContainerEle.scrollLeft === 0) {
+        document.getElementsByClassName('left-arrow')[0].classList.add('hidden')
+    } else if (innerContainerEle.scrollLeft >= 2995) {
+        document.getElementsByClassName('right-arrow')[0].classList.add('hidden')
+    } else if (innerContainerEle.scrollLeft < 2995 && innerContainerEle.scrollLeft >= 0) {
+        document.getElementsByClassName('right-arrow')[0].classList.remove('hidden')
+    }
 }
